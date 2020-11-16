9e9bc5986fca59f2114346c882e7adab42162c21
hugosal <54606569+hugosal@users.noreply.github.com>
2020-11-15 18:56:58 -0600

    ajuste a error en documentacion

--- ecolTest/ecol_creator.R
+++ ecolTest/ecol_creator.R
@@ -1,13 +1,15 @@
-setwd("C:/Users/user/Desktop/R/ecolTest/")
+setwd("C:/Users/user/Desktop/R/THutcheson/Hutcheson-T-test/trunk/ecolTest")
 library(roxygen2)
 library("devtools")
 create("ecolTest")
 setwd("./ecolTest/")
 document()
 setwd("..")
+
 install("ecolTest")
 
 library("ecolTest")
 
 help(Hutcheson.T.test)
 
+Hutcheson.T.test(x=c(1,2,3,4,5),y = c(4,5,7,8,9))
