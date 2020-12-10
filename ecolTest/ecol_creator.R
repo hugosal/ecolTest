96d57ce599dda26aa78f98714f91e1920088b113
MacrofagoTh1like <74377349+MacrofagoTh1like@users.noreply.github.com>
2020-12-10 12:27:58 -0800

    Update ecol_creator.R
    1) se cambió la linea 1, para indicar al voluntario de prueba que debe de asignar como directorio de trabajo aquel donde se haya descargado la carpeta del programa
    2) se eliminaron las líneas de activación de las librerías roxygen2, create("ecolTest") y document(), para evitar que en la prueba lo corrieran de nuevo y diera problemas de sobreescritura en el paquete
    3) se eliminaron los ejemplos para que se pruebe contra el archivo csv de prueba.
    4) Se compartamentalizó del script de prueba para guiar al voluntario en el desarrollo de la prueba
    5) se agregó las indicaciones para que realicen una prueba con un csv de datos reales, Usé el archivo de bentos de la región norte.

--- ecolTest/ecol_creator.R
+++ ecolTest/ecol_creator.R
@@ -1,19 +1,21 @@
-setwd("C:/Users/user/Desktop/R/THutcheson/Hutcheson-T-test/trunk/ecolTest")
-#library("roxygen2")
+s# parte 1. Instlación del paquete para prueba
+setwd("C:/Users/user/Desktop/R/THutcheson/Hutcheson-T-test/trunk/ecolTest") # dar la dirección donde  se descargó el fichero del paquete "ecoltest"
 library("devtools")
-#create("ecolTest")
 setwd("./ecolTest/")
-#document()
 setwd("..")
-
 install("ecolTest")
-
 library("ecolTest")
-
+# como verificación de que se instaló correctamente el paquete prueba, comprobar que se carque la ayuda: 
 help(Hutcheson.T.test)
-
-Hutcheson.T.test(x=c(1,2,3,4,5),y = c(4,5,7,8,9))
-
 help(multiple.Hutcheson.T.test)
-
-multiple.Hutcheson.T.test(iris[,1:3])#prueba sin ningun sentido, solo para probar funcion
+#=============================================================
+# parte 2.  PRUEBA CON DATOS REALES
+#   2.1. Prueba para la función Hutcheson-T-test. Esta función calcula el p-val de Hutcheson T para los pares VAij
+# cargar el archivo de prueba: prueba1.csv
+# correr la función comparando por pares las VA's, ejemplo de sintaxis
+# Hutcheson.T.test(data.frame$VAi,data.frame$VAj,alternative = "g, l, default")
+#-------------------------------------------------------------
+#   parte 2.2. Hutcheson T test múltiple.
+# NOTA: el data frame debe de contener solo objestos numéricos
+#ejemplo de sintaxis
+# multiple.Hutcheson.T.test(nombre de data.frame con solo objetos numéricos)
